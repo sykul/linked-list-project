9a9c8ded5e8817e5a4900db2e31bb164ca575497
sykul <luke.flatley@outlook.com>
2024-09-29 21:35:19 +0200

    first commit

---
+++ LinkedList.js
@@ -0,0 +1,61 @@
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    this.append(value) {
+
+    }
+
+    this.prepend(value) {
+
+    }
+
+    get size() {
+        // return total number of nodes in list
+    }
+
+    get head() {
+        // returns first node in list
+    }
+
+    get tail() {
+        // returns first node in list
+    }
+
+    this.at(index) {
+        // returns the node at the given index
+    }
+
+    this.pop() {
+        // removes last element
+    }
+
+    this.contains(value) {
+        // returns true if list contains value
+    }
+
+    this.find(value) {
+        // return index of node containing value, or null if not found
+    }
+
+    this.toString() {
+        // represents LinkedList objects as strings
+        // format: ( value ) -> ( value ) -> ( value ) -> null
+    }
+
+    // extra credit
+
+    insertAt(value, index) {
+        // inserts a node with 'value' at 'index'
+    }
+
+    removeAt(index) {
+        // removes the node at 'index'
+    }
+
+}
+
+export { LinkedList }
